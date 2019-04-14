1e7fc86070f4a68ddd62de21a2d355388d266f7a
Daniel Greinke <d.greinke@gmail.com>
2019-04-14 16:49:53 -0700

    Add feature: donation ID, supporter amount

--- goFindStats.py
+++ goFindStats.py
@@ -15,11 +15,18 @@ except:
 	print('Could not find button')
 	
 supporters_list = browser.find_element(By.XPATH, "//*[@id='view-donations-modal']")
-donations = supporters_list.find_elements(By.CLASS_NAME,"supporter-amount")
-#supporter_name = supporters_list.find_elements(By.CLASS_NAME, 'supporter-name').text
-donors = []
-print('viewing donors list')
-for person in range(len(donations)):
-	donors.append({"amount": donations[person].text})
-	print(donations[person].text)
-	#print(str(donors))
+donations_post = supporters_list.find_element(By.CSS_SELECTOR,"div.supporter.js-donation-content")
+donation_id = donations_post.get_attribute("data-id")
+donations = supporters_list.find_element(By.CLASS_NAME,"supporter-amount")
+
+print(donation_id)
+print(donations.text)
+
+# donors = []
+# print('viewing donors list')
+# for person in range(len(donations)):
+# 	donors.append({donation_id[person]: donations[person].text})
+# 	print(donations[person].text)
+# 	#print(str(donors))
+
+#html body.theme-gfmgreen.sticky-header.lang-en_US.is-reveal-open div.reveal-overlay.reveal-overlay--flex div#view-donations-modal.gfm-modal-unstyled-wrapper.modal-donations div.gfm-modal.gfm-modal--unstyled div.row.js-donations-contain.collapse div.column.showcontrol-donations div.supporters-list div.supporter.js-donation-content
