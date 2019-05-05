5be32c37fd506f750a76cc2a2a5b425a43766328
Daniel Greinke <d.greinke@gmail.com>
2019-05-05 16:20:33 -0700

    Organize code into functions

--- goFindStats.py
+++ goFindStats.py
@@ -4,55 +4,59 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time, pyautogui, csv, re
 
-browser = webdriver.Firefox()
-timestamp = time.strftime("%Y%m%d%H%M%S")
-browser.get('https://www.gofundme.com/safe-embarcadero-for-all')
-time.sleep(3)
+def main():
+	navigateSite()
+	getDonorCount()
+	openDonorList()
+	scrollDonorList()
+	saveDonorCSV()
+	browser.close()
 
-donor_count_string = browser.find_element(By.XPATH, "(//div[@class='campaign-status text-small'])[2]").text
-donor_regex = re.compile(r"(\d{1,3}(,\d{3})*)")
-donor_count_string2 = donor_regex.search(donor_count_string).group(1)
-count_array = donor_count_string2.split(",")
-donor_count = int(''.join(count_array))
+def navigateSite():
+	global browser
+	browser = webdriver.Firefox()
+	browser.get('https://www.gofundme.com/safe-embarcadero-for-all')
+	time.sleep(3)
 
-try:
+def getDonorCount():
+	donor_count_string = browser.find_element(By.XPATH, "(//div[contains(@class,'campaign-status text-small')])[2]").text
+	donor_regex = re.compile(r"(\d{1,3}(,\d{3})*)")
+	donor_count_string2 = donor_regex.search(donor_count_string).group(1)
+	count_array = donor_count_string2.split(",")
+	global donor_count
+	donor_count = int(''.join(count_array))
+
+def openDonorList():
 	see_more = browser.find_element(By.XPATH, "(//a[contains(@class,'button secondary expanded hide-in-modal')])[1]") #"(//a[contains(.,'See More')])[2]")
 	see_more.click()
-	print('See more')
-except:
-	print('Could not find button')
+	global supporters_list
+	supporters_list = browser.find_element(By.XPATH, "//*[@id='view-donations-modal']")
 
-
-supporters_list = browser.find_element(By.XPATH, "//*[@id='view-donations-modal']")
-
-
-i = 0
-
-while i < (donor_count/10):
+def scrollDonorList():
+	i = 0
+	num_donors = donor_count
+	while i < (num_donors/10):
+		time.sleep(5)
+		pyautogui.scroll(-100)
+		i+=1
 	time.sleep(5)
-	pyautogui.scroll(-100)
-	i+=1
 
-time.sleep(5)
-donations_posts = supporters_list.find_elements(By.CSS_SELECTOR,"div.supporter.js-donation-content")
+def getDonations():
+	donations_posts = supporters_list.find_elements(By.CSS_SELECTOR,"div.supporter.js-donation-content")
+	global donors
+	donors = []
+	for person in range(len(donations_posts)):
+		donation_id = donations_posts[person].get_attribute("data-id")
+		donation_amount = donations_posts[person].find_element(By.CLASS_NAME,"supporter-amount").text
+		donors.append([donation_id, donation_amount])
 
-donors = []
+def saveDonorCSV():
+	getDonations()
+	timestamp = time.strftime("%Y%m%d%H%M%S")
+	results = open("gofundme_query_" + str(timestamp) + ".csv", "w", newline='')
+	outputWriter = csv.writer(results)
+	for row in range(len(donors)):
+		outputWriter.writerow(donors[row])
+	results.close()
 
-for person in range(len(donations_posts)):
-	donation_id = donations_posts[person].get_attribute("data-id")
-	donation_amount = donations_posts[person].find_element(By.CLASS_NAME,"supporter-amount").text
-	#print(donation_id)
-	#print(donation_amount)
-	donors.append([donation_id, donation_amount])
-
-results = open("gofundme_query_" + str(timestamp) + ".csv", "w", newline='')
-outputWriter = csv.writer(results)
-
-for row in range(len(donors)):
-	outputWriter.writerow(donors[row])
-
-results.close()
-
-# #TODO: 
-# 	# -Move everything into functions
-# 	# -Stop using absolute XPATH for see more button
+main()
