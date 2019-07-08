aad637bb584acacb735ad22dde379a050cb3c511
Dan Greinke <d.greinke@gmail.com>
2019-07-07 20:38:42 -0700

    Implement SystemRandom

--- code-jam-5-qualifier/qualifier.py
+++ code-jam-5-qualifier/qualifier.py
@@ -1,10 +1,10 @@
-import random
+import random, sys
 
 
 def generate_password(
-    password_length: int = 8,
-    has_symbols: bool = False,
-    has_uppercase: bool = False
+    password_length: int = 16,
+    has_symbols: bool = True,
+    has_uppercase: bool = True
 ) -> str:
     password = randomize_pos(password_length, has_symbols, has_uppercase)
     print(password)
@@ -24,18 +24,22 @@ def generate_password(
     # Digits: 48 to 57
     # Capital : 65 through 90
     # lower case: 97 to 122
+    
+def sysrnd():
+    rnd = random.SystemRandom()
+    return rnd
 
 def select_special():
     special_chars = [i for j in (range(32,48), range(58,65), range(91,97), range(123,127)) for i in j]
-    random_char = random.randint(0,27)
+    random_char = sysrnd().randint(0,27)
     return chr(special_chars[random_char])
 
 def select_upper():
-    upper_case = chr(random.randint(65,91))
+    upper_case = chr(sysrnd().randint(65,91))
     return upper_case
 
 def select_any():
-    any_char = chr(random.randint(32,127))
+    any_char = chr(sysrnd().randint(32,127))
     #print(str(type(any_char)))
     #print(str(any_char))
     return any_char
@@ -61,17 +65,17 @@ def rough_generate(length, sym, uppr):
 def randomize_pos(length, sym, uppr):
     password = rough_generate(length, sym, uppr)
     if sym == True:
-        sym_index = random.randint(0,length-1)
+        sym_index = sysrnd().randint(0,length-1)
         password.insert(sym_index, select_special())
         length += 1
         if uppr == True:
-            uppr_index = random.randint(0,length-1) 
+            uppr_index = sysrnd().randint(0,length-1) 
             password.insert(uppr_index, select_upper())
         else:
             pass
     else:
         if uppr == True:
-            uppr_index = random.randint(0,length-1) 
+            uppr_index = sysrnd().randint(0,length-1) 
             password.insert(uppr_index, select_upper())
         else:
             pass
