31ac3c94962d0a375755e90103fcefcd381a2879
DanGreinke <d.greinke@gmail.com>
2019-11-17 12:22:25 -0800

    remove function

--- get_weather.py
+++ get_weather.py
@@ -14,34 +14,37 @@ current_weather_data = json.loads(current_weather.text)
 forecast_weather = requests.get('https://api.openweathermap.org/data/2.5/forecast?q=San+Francisco,us&appid={}'.format(api_key))
 forecast_data = json.loads(forecast_weather.text)
 
-def get_current_weather():
-    last_update = []
-    last_update_time = time.ctime(current_weather_data['dt'])
-    current_temp = current_weather_data['main']['temp'] - 273.15
-    temp_str = ("%.2f" %current_temp) + " C"
-    weather_now = current_weather_data['weather'][0]['main']
-    last_update = [last_update_time, temp_str, weather_now]
-    return last_update
 
-def get_forecast_time(sample):
-    sample_epoch = forecast_data['list'][sample]['dt']
+def get_time(sample, forecast):
+    if forecast == True:
+        sample_epoch = forecast_data['list'][sample]['dt']
+    else:
+        sample_epoch = current_weather_data['dt']
+
     sample_time = time.ctime(sample_epoch)
     return sample_time
 
-def get_forecast_temp(sample):
-    sample_temp = forecast_data['list'][sample]['main']['temp'] - 273.15
+def get_temp(sample, forecast):
+    if forecast == True:
+        sample_temp = forecast_data['list'][sample]['main']['temp'] - 273.15
+    else:
+        sample_temp = current_weather_data['main']['temp'] - 273.15
     sample_temp_str = ("%.2f" %sample_temp) + " C"
     return sample_temp_str
 
-def get_forecast_weather(sample):
+def get_weather(sample, forecast):
+    if forecast == True:
+        sample_weather = forecast_data['list'][sample]['weather'][0]['main']
+    else:
+        sample_weather = current_weather_data['weather'][0]['main']
     sample_weather = forecast_data['list'][sample]['weather'][0]['main']
     return sample_weather
 
 def main():
     print("Location: " + forecast_data['city']['name'])
-    print(get_current_weather()[0] + " .... " + "Temp: " + get_current_weather()[1] + " .... " + get_current_weather()[2])
+    print(get_time(-1,False) + " .... " + "Temp: " + get_temp(-1, False) + " .... " + get_weather(-1, False))
 
     for bin in range(0,9):
-        print(get_forecast_time(bin) + " .... " + "Temp: " + get_forecast_temp(bin) + " .... " + get_forecast_weather(bin))
+        print(get_time(bin, True) + " .... " + "Temp: " + get_temp(bin, True) + " .... " + get_weather(bin, True))
 
 main()
