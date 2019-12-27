802777d77f87782444d698e5af752d45661ab025
DanGreinke <d.greinke@gmail.com>
2019-12-26 17:18:44 -0800

    Return datetime object, fulfill basic requirements

--- code-jam-6-qualifier-master/qualifier.py
+++ code-jam-6-qualifier-master/qualifier.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import time
 
 
 def parse_iso8601(timestamp: str) -> datetime.datetime:
@@ -29,32 +30,39 @@ def parse_iso8601(timestamp: str) -> datetime.datetime:
         second = result.group('second')
 
     my_time = []
+    #print(my_time)
     for result in year, month, day, hour, minute, second:
         if result != None:
-            my_time.append(result)
-    print(my_time)
+            my_time.append(int(result))
+        else:
+            my_time.append(0)
+    #print(my_time)
     max_time = [9999, 12, 31, 23, 59, 59]
     for time in range(len(my_time)):
-        if int(my_time[time]) > max_time[time]:
+        if my_time[time] > max_time[time]:
             raise ValueError("Input timestamp is not valid. " + str(my_time[time])
              + " > " + str(max_time[time]))
 
     time_str = ""
     for time in range(len(my_time)):
-        time_str += my_time[time] + ","
-    print(time_str)
+        time_str += str(my_time[time]) + ","
+    #print(time_str)
 
-    print("Year: " + str(year) + "\n"
-          "Month: " + str(month) + "\n"
-          "Day: " + str(day) + "\n"
-          "Hour: " + str(hour) + "\n"
-          "Minute: " + str(minute) + "\n"
-          "Second: " + str(second)
-    )
+    # print("Year: " + str(year) + "\n"
+    #       "Month: " + str(month) + "\n"
+    #       "Day: " + str(day) + "\n"
+    #       "Hour: " + str(hour) + "\n"
+    #       "Minute: " + str(minute) + "\n"
+    #       "Second: " + str(second)
+    # )
+    my_dt = datetime.datetime(my_time[0], my_time[1], my_time[2], my_time[3],
+                            my_time[4], my_time[5])
+    #print(my_dt)
+    return my_dt
+
 
 test_string = input("Enter a timestamp: ")
 parse_iso8601(test_string)
-
 #add functionality to check for valid date, timestamp
 
 #add fractional seconds
