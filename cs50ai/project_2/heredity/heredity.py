b0b97dc897e47671778a8e770811b96a771ae803
DanGreinke <d.greinke@gmail.com>
2023-02-12 16:12:10 -0800

    remove comment

--- cs50ai/project_2/heredity/heredity.py
+++ cs50ai/project_2/heredity/heredity.py
@@ -228,13 +228,3 @@ def normalize(probabilities):
 
 if __name__ == "__main__":
     main()
-
-"""
-from heredity import *
-people = {
-   'Harry': {'name': 'Harry', 'mother': 'Lily', 'father': 'James', 'trait': None},
-   'James': {'name': 'James', 'mother': None, 'father': None, 'trait': True},
-   'Lily': {'name': 'Lily', 'mother': None, 'father': None, 'trait': False}
-}
-joint_probability(people, {"Harry"}, {"James"}, {"James"})
-"""
