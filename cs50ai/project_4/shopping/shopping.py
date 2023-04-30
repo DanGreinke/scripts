f71d454c331d9e6e0cf3d6a5011fbb2ff3c63a7c
DanGreinke <d.greinke@gmail.com>
2023-04-30 13:38:34 -0700

    complete shopping assignment

--- cs50ai/project_4/shopping/shopping.py
+++ cs50ai/project_4/shopping/shopping.py
@@ -31,6 +31,39 @@ def main():
     print(f"True Negative Rate: {100 * specificity:.2f}%")
 
 
+def month2num(month):
+    m = month[:3]
+    months = {
+        "Jan":0,
+        "Feb":1,
+        "Mar":2,
+        "Apr":3,
+        "May":4,
+        "Jun":5,
+        "Jul":6,
+        "Aug":7,
+        "Sep":8,
+        "Oct":9,
+        "Nov":10,
+        "Dec":11
+    }
+    return months[m]
+
+def bool2int(data):
+    d = {
+            "TRUE":1,
+            "FALSE":0
+        }
+    return d[data]
+
+def visitor2int(visitor):
+    v = {
+            "Returning_Visitor":1,
+            "New_Visitor":0,
+            "Other":0
+        }
+    return v[visitor]
+
 def load_data(filename):
     """
     Load shopping data from a CSV file `filename` and convert into a list of
@@ -59,16 +92,44 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
-
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        evidence = list()
+        labels = list()
+        
+        for row in reader:
+            # print(row)
+            evidence.append([
+                                int(row[0]),
+                                float(row[1]),
+                                int(row[2]),
+                                float(row[3]),
+                                int(row[4]),
+                                float(row[5]),
+                                float(row[6]),
+                                float(row[7]),
+                                float(row[8]),
+                                float(row[9]),
+                                month2num(row[10]),
+                                int(row[11]),
+                                int(row[12]),
+                                int(row[13]),
+                                int(row[14]),
+                                visitor2int(row[15]),
+                                bool2int(row[16])
+                            ])    
+            # print(new_evidence)
+            labels.append(bool2int(row[17]))
+        return (evidence, labels)
 
 def train_model(evidence, labels):
     """
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
-
+    model = KNeighborsClassifier(n_neighbors=1)
+    return model.fit(evidence,labels)
 
 def evaluate(labels, predictions):
     """
@@ -85,7 +146,29 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    true_positives = 0
+    true_negatives = 0
+    false_negatives = 0
+    false_positives = 0
+
+    for label, prediction in zip(labels, predictions):
+        # True Positive
+        if label == prediction == 1:
+            true_positives += 1
+        # True Negative
+        elif label == prediction == 0:
+            true_negatives += 1
+        # False Negative
+        elif label == 1 and prediction == 0:
+            false_negatives += 1
+        # Flase Positive
+        elif label == 0 and prediction ==1:
+            false_positives += 1
+    
+    sensitivity = true_positives / (true_positives + false_negatives) 
+    specificity = true_negatives / (false_positives + true_negatives)
+
+    return (sensitivity,specificity)
 
 
 if __name__ == "__main__":
